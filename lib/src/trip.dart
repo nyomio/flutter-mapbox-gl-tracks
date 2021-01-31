08ff23716c07ebe6be0cb5f247a891c91885dd98
Nagy István <>
2021-01-31 17:32:39 +0100

    Waiting time & equals

--- lib/src/trip.dart
+++ lib/src/trip.dart
@@ -1,30 +1,48 @@
 
 class Trip {
-  int id;
-  String color;
-  List<GpsLocation> coordinates;
+  final int id;
+  final String color;
+  final List<GpsLocation> coordinates;
+  final int waitingTime;
 
-  Trip(int tripId, String tripColor, List<GpsLocation> tripCoordinates) {
-    this.id = tripId;
-    this.coordinates = tripCoordinates;
-    this.color = tripColor;
-  }
+  Trip(this.id, this.color, this.coordinates, this.waitingTime);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is Trip &&
+              runtimeType == other.runtimeType &&
+              id == other.id &&
+              color == other.color &&
+              coordinates == other.coordinates &&
+              waitingTime == other.waitingTime;
+
+  @override
+  int get hashCode => id.hashCode ^ color.hashCode ^ coordinates.hashCode ^ waitingTime.hashCode;
 }
 
 class GpsLocation {
-  double lat;
-  double long;
-  double heading;
-  double alt;
-  double speed;
-  int time;
+  final double lat;
+  final double long;
+  final double alt;
+  final double heading;
+  final double speed;
+  final int time;
 
-  GpsLocation(double lat, double long, double heading, double alt, double speed, int time) {
-    this.lat = lat;
-    this.long = long;
-    this.heading = heading;
-    this.alt = alt;
-    this.speed = speed;
-    this.time = time;
-  }
+  GpsLocation(this.lat, this.long, this.alt, this.heading, this.speed, this.time);
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+          other is GpsLocation &&
+              runtimeType == other.runtimeType &&
+              lat == other.lat &&
+              long == other.long &&
+              alt == other.alt &&
+              heading == other.heading &&
+              speed == other.speed &&
+              time == other.time;
+
+  @override
+  int get hashCode => lat.hashCode ^ long.hashCode ^ alt.hashCode ^ heading.hashCode ^ speed.hashCode ^ time.hashCode;
 }
